4cc1d871dcd84261ae7977cbdde2cb28506b4adb
perluto <nhhoanguet@gmail.com>
2020-12-06 19:47:39 +0700

    fix css component postDetail and postShortcut

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./components/navBar";
-import PostForm from "./components/postForm/postForm.jsx";
-import PostShotCut from "./components/postShortcut";
+import PostResForm from "./components/postResForm/postResForm.jsx";
+import PostShortCut from "./components/postShortcut";
 import { Switch, Route } from "react-router-dom";
 import postDetail from "./components/postDetail";
 class App extends Component {
@@ -12,8 +12,8 @@ class App extends Component {
         <NavBar></NavBar>
         <main className="container">
           <Switch>
-            <Route path="/postForm" component={PostForm}></Route>
-            <Route path="/postShotcut" component={PostShotCut}></Route>
+            <Route path="/postForm" component={PostResForm}></Route>
+            <Route path="/postShortcut" component={PostShortCut}></Route>
             <Route path="/postDetail" component={postDetail}></Route>
           </Switch>
         </main>
--- src/components/postDetail.jsx
+++ src/components/postDetail.jsx
@@ -1,101 +1,113 @@
-import React,{Component} from "react";
-import "../style/postDetail.css"
+import React, { Component } from "react";
 import Image1 from "../image/teamb1.png";
 import Image2 from "../image/teamb2.jpg";
 import Image3 from "../image/teamb3.jpg";
 
-class postDetail extends Component{
-    state ={
-        name: 'Nguyen Duc Anh',
-        address: '158/65 Đường số 9, Phường 9, Quận Gò Vấp, Hồ Chí Minh',
-        phone: 913696081,
-        price: 2,
-        acreage: 18,
-        expiration:"06/12/20",
-        update: "01/12/20",
-        image3: Image3,
-        image1: Image1,
-        image2: Image2
-    };
+class postDetail extends Component {
+  state = {
+    name: "Nguyen Duc Anh",
+    address: "158/65 Đường số 9, Phường 9, Quận Gò Vấp, Hồ Chí Minh",
+    phone: 913696081,
+    price: 2,
+    acreage: 18,
+    expiration: "06/12/20",
+    update: "01/12/20",
+    image3: Image3,
+    image1: Image1,
+    image2: Image2,
+  };
 
-    render(){
-        return(
-            <div className="container">
-            <table className="table table-bordered table-detail">
-                <tbody>
-                    <tr>
-                        <td className="name">Địa chỉ:</td>
-                        <td className="content">{this.state.address}</td>
-                    </tr>
-                    <tr>
-                        <td className="name">Người đăng:</td>
-                        <td className="content">{this.state.name}</td>
-                    </tr>
-                    <tr>
-                        <td className="name">Điện thoại:</td>
-                        <td className="content">{this.state.phone}</td>
-                    </tr>
-                    <tr>
-                        <td className="name">Diện tích:</td>
-                        <td className="content">{this.state.acreage}</td>
-                    </tr>
-                    <tr>
-                        <td className="name">Giá:</td>
-                        <td className="content">{this.state.price}</td>
-                    </tr>
-                    <tr>
-                        <td className="name">Ngày cập nhật:</td>
-                        <td className="content">{this.state.update}</td>
-                    </tr>
-                    <tr>
-                        <td className="name">Ngày hết hạn:</td>
-                        <td className="content">{this.state.expiration}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className="des">
-                <strong>Thông tin mô tả</strong>
-                <p>-Phòng tắm:</p>
-                <p>-Phòng bếp:</p>
-                <p>-Điều hòa:</p>
-                <p>-Ban công:</p>
-                <p>-Điện nước:</p>
-                <p>-Tiện ích:</p>
-            </div>
-            <div className="slide">
-            <div id="demo" class="carousel slide" data-ride="carousel">
-
-                <ul class="carousel-indicators">
-                    <li data-target="#demo" data-slide-to="0" class="active"></li>
-                    <li data-target="#demo" data-slide-to="1"></li>
-                    <li data-target="#demo" data-slide-to="2"></li>
-                </ul>
-
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img className="ima" src={this.state.image1} alt="Los Angeles"/>
-                    </div>
-                    <div class="carousel-item">
-                        <img className="ima" src={this.state.image2} alt="Chicago"/>
-                    </div>
-                    <div class="carousel-item">
-                        <img className="ima" src={this.state.image3} alt="New York"/>
-                    </div>
-                </div>
-
-                <a class="carousel-control-prev" href="#demo" data-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
-                </a>
-                <a class="carousel-control-next" href="#demo" data-slide="next">
-                    <span class="carousel-control-next-icon"></span>
-                </a>
-
-                </div>
-            </div>
+  render() {
+    return (
+      <div className="container">
+        <table className="table table-bordered w-75">
+          <tbody>
+            <tr>
+              <td className="bg-light">Địa chỉ:</td>
+              <td className="w-75">{this.state.address}</td>
+            </tr>
+            <tr>
+              <td className="bg-light">Người đăng:</td>
+              <td className="w-75">{this.state.name}</td>
+            </tr>
+            <tr>
+              <td className="bg-light">Điện thoại:</td>
+              <td className="w-75">{this.state.phone}</td>
+            </tr>
+            <tr>
+              <td className="bg-light">Diện tích:</td>
+              <td className="w-75">{this.state.acreage}</td>
+            </tr>
+            <tr>
+              <td className="bg-light">Giá:</td>
+              <td className="w-75">{this.state.price}</td>
+            </tr>
+            <tr>
+              <td className="bg-light">Ngày cập nhật:</td>
+              <td className="w-75">{this.state.update}</td>
+            </tr>
+            <tr>
+              <td className="bg-light">Ngày hết hạn:</td>
+              <td className="w-75">{this.state.expiration}</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="border rounded p-2 w-75 mb-2">
+          <h5 class="text-info">Thông tin mô tả</h5>
+          <div></div>
         </div>
-            
-        )
-    }
+        <div className="w-75">
+          <h5 class="text-info">Hinh anh</h5>
+          <div id="slide" class="carousel slide" data-ride="carousel">
+            <ul class="carousel-indicators">
+              <li data-target="#slide" data-slide-to="0" class="active"></li>
+              <li data-target="#slide" data-slide-to="1"></li>
+              <li data-target="#slide" data-slide-to="2"></li>
+            </ul>
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  src={this.state.image1}
+                  className="d-block w-100"
+                  alt="Image1"
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src={this.state.image2}
+                  className="d-block w-100"
+                  alt="Image2"
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src={this.state.image3}
+                  className="d-block w-100"
+                  alt="Image3"
+                />
+              </div>
+            </div>
+
+            <a class="carousel-control-prev" href="#slide" data-slide="prev">
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#slide" data-slide="next">
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default postDetail;
